1e6842695599d291b6f83bff3317112ae94bef12
Dion Jansen <dion.jansen@capgemini.com>
2017-02-10 22:36:18 +0100

    added used js

---
+++ cs-spulletjes/print.js
@@ -0,0 +1,6 @@
+function retrieved(type, color, coords) {
+	console.log("Grabbed a cube by the P*")
+	console.log("Type", type);
+	console.log("Color", color);
+	console.log("Coordinates", coords);
+}
